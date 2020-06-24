7487cfe0b957358286a23425fa9472a5bb758daa
Gota <guotianxiang@chachazhan.com>
2020-06-24 15:06:41 +0800

    Add RedisV8 driver

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/GotaX/go-server-skeleton
 
-go 1.13
+go 1.14
 
 require (
 	contrib.go.opencensus.io/exporter/jaeger v0.2.0
@@ -16,10 +16,11 @@ require (
 	github.com/gin-gonic/gin v1.5.0
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-redis/redis/v7 v7.0.0-beta.4
+	github.com/go-redis/redis/v8 v8.0.0-beta.5 // indirect
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/gogo/protobuf v1.3.1 // indirect
 	github.com/golang/groupcache v0.0.0-20191027212112-611e8accdfc9 // indirect
-	github.com/golang/protobuf v1.3.2
+	github.com/golang/protobuf v1.3.3
 	github.com/grpc-ecosystem/go-grpc-middleware v1.1.0
 	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0
 	github.com/json-iterator/go v1.1.8 // indirect
@@ -39,11 +40,11 @@ require (
 	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e
 	golang.org/x/sys v0.0.0-20191127021746-63cb32ae39b2 // indirect
 	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
-	golang.org/x/xerrors v0.0.0-20191011141410-1b5146add898
+	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543
 	google.golang.org/api v0.14.0 // indirect
 	google.golang.org/appengine v1.6.5 // indirect
 	google.golang.org/genproto v0.0.0-20191115221424-83cc0476cb11
-	google.golang.org/grpc v1.25.1
+	google.golang.org/grpc v1.29.1
 	gopkg.in/go-playground/validator.v9 v9.30.2 // indirect
 	gopkg.in/ini.v1 v1.51.0 // indirect
 	gopkg.in/yaml.v2 v2.2.7 // indirect
--- pkg/cfg/factory/redis.go
+++ pkg/cfg/factory/redis.go
@@ -3,28 +3,53 @@ package factory
 import (
 	"fmt"
 
-	driver "github.com/go-redis/redis/v7"
+	v7 "github.com/go-redis/redis/v7"
+	v8 "github.com/go-redis/redis/v8"
 )
 
-var Redis = Option{
-	Name:     "Redis",
-	OnCreate: newRedis,
-}
+const (
+	_ = iota
+	redisV7
+	redisV8
+)
 
-func newRedis(source Scanner) (interface{}, error) {
-	var c struct {
-		Db       int    `json:"db"`
-		Host     string `json:"host"`
-		Password string `json:"password"`
-		Port     string `json:"port"`
+var (
+	Redis = Option{
+		Name:     "Redis",
+		OnCreate: newRedis(redisV7),
 	}
-	if err := source.Scan(&c); err != nil {
-		return nil, err
+	RedisV8 = Option{
+		Name:     "Redis",
+		OnCreate: newRedis(redisV8),
 	}
+)
 
-	return driver.NewClient(&driver.Options{
-		DB:       c.Db,
-		Addr:     fmt.Sprintf("%v:%v", c.Host, c.Port),
-		Password: c.Password,
-	}), nil
+func newRedis(version int) func(source Scanner) (interface{}, error) {
+	return func(source Scanner) (obj interface{}, err error) {
+		var c struct {
+			Db       int    `json:"db"`
+			Host     string `json:"host"`
+			Password string `json:"password"`
+			Port     string `json:"port"`
+		}
+		if err := source.Scan(&c); err != nil {
+			return nil, err
+		}
+
+		switch version {
+		case redisV7:
+			obj = v7.NewClient(&v7.Options{
+				DB:       c.Db,
+				Addr:     fmt.Sprintf("%v:%v", c.Host, c.Port),
+				Password: c.Password,
+			})
+		case redisV8:
+			obj = v8.NewClient(&v8.Options{
+				DB:       c.Db,
+				Addr:     fmt.Sprintf("%v:%v", c.Host, c.Port),
+				Password: c.Password,
+			})
+		}
+		return
+	}
 }
