6d3e312096d2eba13e61783efd1b10160d098c07
Gota <guotianxiang@chachazhan.com>
2021-04-27 23:41:59 +0800

    Set dapr request headers by incoming context

--- pkg/clients/dapr.go
+++ pkg/clients/dapr.go
@@ -3,8 +3,11 @@ package clients
 import (
 	"context"
 	"fmt"
+	"net/url"
+	"strings"
 
 	"github.com/go-resty/resty/v2"
+	"google.golang.org/grpc/metadata"
 
 	"github.com/GotaX/go-server-skeleton/pkg/errors"
 )
@@ -30,9 +33,11 @@ type daprHttp struct {
 func (c *daprHttp) Invoke(ctx context.Context, service, method string, req, resp interface{}) error {
 	addr := fmt.Sprintf("%s/invoke/%s/method/%s", c.Endpoint, service, method)
 
+	headers := headersFromContext(ctx)
+
 	response, err := c.Client.R().
 		SetContext(ctx).
-		SetHeader("Content-Type", "application/json").
+		SetHeaders(headers).
 		SetBody(req).
 		SetResult(&resp).
 		Post(addr)
@@ -44,4 +49,16 @@ func (c *daprHttp) Invoke(ctx context.Context, service, method string, req, resp
 		return errors.FromHttp(response.RawResponse)
 	}
 	return nil
+}
+
+func headersFromContext(ctx context.Context) map[string]string {
+	headers := map[string]string{"Content-Type": "application/json"}
+
+	if md, ok := metadata.FromOutgoingContext(ctx); ok {
+		for key, values := range md {
+			content := strings.Join(values, ";")
+			headers[key] = url.QueryEscape(content)
+		}
+	}
+	return headers
 }
